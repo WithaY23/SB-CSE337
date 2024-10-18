f137448bc7928fc3668a4f8b796a0679d65e657a
Ayden <aebudhoo@aol.com>
2024-10-18 10:55:29 -0400

    Finished overriding map function of Array
    *+Uses clean and neat code
    *+Fulfills test cases
    	*Tested Out Of Bounds, negative to positive, positive to negative, positive to negative OB
    *-Slightly more testing needed
    *-Remove unnecessary comments
    	*Move Previous testing to another file
    *-Document test cases more thoroughly

--- hw2.rb
+++ hw2.rb
@@ -13,10 +13,10 @@ class Array
 
       # Ensure the range is valid
       if rangeStart < 0 || rangeStart >= self.size || rangeEnd < 0
-        return '\0' # Return nil for invalid range, following Ruby convention
+        return '\0' # Return '\0' for invalid range
       end
 
-      # Use the original `[]` method to access the range
+      # Use the original access method to access the range
       return oldAccess(index)
        
 
@@ -31,22 +31,39 @@ class Array
     end
   end 
   def map(sequence = nil, &block) #applies block to valid sequences
+    changed = []
     if (sequence.nil?)#check if there was a provided sequence
-      #APPLY TO ALL
+        #
+        APPLY TO ALL
+        self.each do |apply|
+          changed.append(block.call(apply))
+        end
+    return changed
+      
     else #sequence provided
-      if(sequence.begin >= self.size || sequence.begin < -(self.size)) #check if beginning of the range is valid, return fail if not
-        return []
-      else
-
-      
+      if(self[sequence]  != '\0') #check if sequence is valid SEEMS TO WORK, TEST
+        self[sequence].each do |apply|
+          changed.append(block.call(apply))
+        end
       end
-      
-      
-      
 
-
-
+      return changed 
     end
+
+        
+      # if(sequence.begin >= self.size || sequence.begin < -(self.size)) #check if beginning of the range is valid, return fail if not
+      #   return []
+      # else
+
+      
+      # end
+      
+      
+      
+
+
+
+    #end
 
   end
     
@@ -57,14 +74,80 @@ class Array
 end
 
 
+
+
 myArr = [1,2,3,4,5]
 
-myArr.map{|x| x+2}
+
+
+range1 = (2..5)
+range2 = (-5..3)
+range3 = (1..-2)
+range4 = (2..-1)
+range5= (3..-3)
+range6 = (3..-2)
+
+proc1 = Proc.new{|x| x+2}
+proc2 = Proc.new{|x| x * 3}
 
 
 
+puts "Original Array"
+myArr.each do |x| puts x end
+
+newArr = myArr.map(range1,&proc1)
+newArr.each do |x| puts x end
+#Expected: 5 6 7
+#Actual: 5 6 7
 
 
+newArr = myArr.map(range2,&proc2)
+newArr.each do |x| puts x end
+#Expected: 3 6 9 12
+#Actual: 3 6 9 12
+
+puts "Test 3"
+newArr = myArr.map(range3){|x| x.even?}
+newArr.each do |x| puts x end
+#Expected: true false true
+#Actual: true false true
+
+puts "Test 4"
+newArr = myArr.map(range4){|x| x * x}
+newArr.each do |x| puts x end
+#Expected: 9 16 25
+#Actual: 9 16 25
+
+puts "Test 5"
+newArr = myArr.map(range5){|x| x.to_s}
+newArr.each do |x| puts x end
+#Expected:
+#Actual:
+
+
+puts "Test 6"
+newArr = myArr.map(range6){|x| x + 10 }
+newArr.each do |x| puts x end
+#Expected: 14
+#Actual: 14
+
+
+
+puts "Class test cases"
+puts
+a= [1,2,34,5]
+
+puts a[1] # 2
+puts a[10] # '\0'
+p a.map(2..4) { |i| i.to_f} # [34.0, 5.0]
+p a.map { |i| i.to_f} # [1.0, 2.0, 34.0, 5.0]
+b = ["cat", "bat", "mat", "sat"]
+puts b[-1] # "sat"
+puts b[5] # '\0'
+p b.map(2..10) { |x| x[0].upcase + x[1,x.length] } # ["Mat", "Sat"]
+p b.map(2..4) { |x| x[0].upcase + x[1,x.length] } # ["Mat", "Sat"]
+p b.map(-3..-1) { |x| x[0].upcase + x[1,x.length] } # ["Bat", "Mat", “Sat”] 
+p b.map { |x| x[0].upcase + x[1,x.length] } # ["Cat", "Bat", "Mat", "Sat"]
 
 
 
